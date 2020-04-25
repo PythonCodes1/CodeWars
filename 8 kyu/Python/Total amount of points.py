f6e46be55c88c9b3fe66df878318302b4b1ce712
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-04-25 18:08:33 -0400

    Create Total amount of points.py

---
+++ 8 kyu/Python/Total amount of points.py
@@ -0,0 +1,12 @@
+# Calculates the points won from a football championship
+# 8 kyu
+
+def points(games):
+    result = 0
+    for i in games:
+        j = i.split(':')
+        if j[0] > j[1]:
+            result+=3
+        elif j[0] == j[1]:
+            result += 1
+    return result
