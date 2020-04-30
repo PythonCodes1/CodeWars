c7b90061e0d90d20985a0507bb92f289ff22d99d
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-04-30 16:02:59 -0400

    Create The highest profit wins.py

---
+++ 7 kyu/Python/The highest profit wins.py
@@ -0,0 +1,4 @@
+# Write a function that returns both the minimum and maximum number of the given list/array.
+# https://www.codewars.com/kata/559590633066759614000063/train/python
+def min_max(lst):
+  return [min(lst), max(lst)]
