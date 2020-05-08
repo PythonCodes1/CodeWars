829aa5950e67ec70d77958c6fc13fa9de3528429
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-05-08 19:02:00 -0400

    Create Secret Message.py

---
+++ 6 kyu/Python/Secret Message.py
@@ -0,0 +1,9 @@
+def find_secret_message(paragraph):
+    paragraph = ''.join([c for c in paragraph.lower() if c in 'abcdefghijklmnopqrstuvwxyz -'])
+    appear,res = [],[]
+    for word in paragraph.split():
+        if word not in appear:
+            appear.append(word)
+        elif word not in res:
+            res.append(word)
+    return ' '.join(res)
